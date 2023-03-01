c519b5105d205b0ee3b56f6ee6c25ec4bac1d7d4
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-03-01 20:21:00 +0700

    TUGAS

--- 9_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/SOAL PRIORITAS 1 (80)/Program1/main.go
+++ 9_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/SOAL PRIORITAS 1 (80)/Program1/main.go
@@ -8,16 +8,18 @@ import "fmt"
 //pendaklarasian struct
 type Mobil struct {
 	Type   string
-	fuelIn float64 //  untuk menampung bahan bakar yang ada dengan satuan liter
+	fuelin float64 //  untuk menampung bahan bakar yang ada dengan satuan liter
 }
 
 func (mobil Mobil) PerhitunganJarakTempuh() float64 { // PerhitunganJarakTempuh merupakan nama method untuk menghitung jarak yang ditempuh dengan bahan bakar 1.5 liter
-	JarakTempuh := mobil.fuelIn * 1.5
+	JarakTempuh := mobil.fuelin * 1.5
 	return JarakTempuh // variabel untuk menyimpan perhitungan jarak yang dapat ditempuh
 }
 
 func main() {
-	myMobil := Mobil{Type: "Station Wagon", fuelIn: 30.5}
+	myMobil := Mobil{
+		Type:   "Station Wagon",
+		fuelin: 15}
 	JarakTempuh := myMobil.PerhitunganJarakTempuh()
 
 	fmt.Println("Jarak yang dapat ditempuh oleh mobil", myMobil.Type, "dengan bahan bakar yang sedang terisi 1,5 Liter adalah", JarakTempuh, "km")
