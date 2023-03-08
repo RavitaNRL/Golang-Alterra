fd0989a5b4adad6433cc0f6e4ef4a616bbf20301
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-03-08 20:12:13 +0700

    TUGAS

--- 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 3.go
+++ 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 3.go
@@ -1,22 +1,35 @@
+// SOAL
+//Angka Fibonacci adalah serangkaian angka di mana setiap angka adalah jumlah dari keduanya nomor sebelumnya. Beberapa angka Fibonacci pertama adalah: 0, 1, 1, 2, 3, 5, 8, ….
+// Buatlah fungsi untuk menghitung angka Fibonacci ke-n (top-down)!
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func fibona(bil int) int {
-	if bil <= 1 {
-		return bil
+func FiboTopDown(number int, listFibo map[int]int) int {
+	if number <= 1 {
+		return number
 	}
-	return fibona(bil-1) + fibona(bil-2)
-}
 
+	if value, ok := listFibo[number]; ok {
+		return value
+	}
+
+	listFibo[number] = FiboTopDown(number-1, listFibo) + FiboTopDown(number-2, listFibo)
+	return listFibo[number]
+}
 func main() {
-	fmt.Println(fibona(0))  // 0
-	fmt.Println(fibona(1))  // 1
-	fmt.Println(fibona(2))  // 1
-	fmt.Println(fibona(3))  // 2
-	fmt.Println(fibona(5))  // 5
-	fmt.Println(fibona(6))  // 8
-	fmt.Println(fibona(7))  // 13
-	fmt.Println(fibona(9))  // 34
-	fmt.Println(fibona(10)) // 55
+	list := make(map[int]int)
+	fmt.Println(FiboTopDown(0, list))  // 0
+	fmt.Println(FiboTopDown(1, list))  // 1
+	fmt.Println(FiboTopDown(2, list))  // 1
+	fmt.Println(FiboTopDown(3, list))  // 2
+	fmt.Println(FiboTopDown(5, list))  // 5
+	fmt.Println(FiboTopDown(6, list))  // 8
+	fmt.Println(FiboTopDown(7, list))  // 13
+	fmt.Println(FiboTopDown(9, list))  // 34
+	fmt.Println(FiboTopDown(10, list)) // 55
+
 }
