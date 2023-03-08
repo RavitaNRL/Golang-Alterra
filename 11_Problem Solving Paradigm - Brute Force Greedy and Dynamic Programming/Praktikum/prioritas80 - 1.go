7aaf47dd5c76b9fa4eb8c7a2c723053c3e867163
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-03-08 20:33:49 +0700

    TUGAS

--- 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 1.go
+++ 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 1.go
@@ -2,15 +2,14 @@ package main
 
 import "fmt"
 
-func digitBiner(bilBulat int) []int {
-	ans := make([]int, bilBulat+1) // make digunakan untuk menginisialisasikan slice dengan ukuran bilBulat+1.
-	for i := 0; i <= bilBulat; i++ {
-		ans[i] = ans[i>>1] + i&1
+func digitBiner(bilanganBulat int) {
+	for z := 0; z <= bilanganBulat; z++ {
+		fmt.Printf("%b", z)
 	}
-	return ans
 }
 
 func main() {
-	fmt.Println(digitBiner(2)) // [0 1 1]
-	fmt.Println(digitBiner(8)) // [0 1 1 2 1 2 2 3 1]
+	digitBiner(2) // [0 1 1 0]
+	fmt.Println()
+	digitBiner(3) // [0 1 10 11]
 }
