53e58cbd2afd29dd1a89326320ebc0080055e226
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-03-08 20:34:48 +0700

    TUGAS

--- 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 1.go
+++ 11_Problem Solving Paradigm - Brute Force Greedy and Dynamic Programming/Praktikum/prioritas80 - 1.go
@@ -1,3 +1,14 @@
+// SOAL
+// - Diberi bilangan bulat n, kembalikan array ans dengan panjang n + 1 sehingga untuk setiap i (0 <= i <= n), ans[i] adalah bilangan 1 dalam representasi biner dari i
+
+// Input: n = 2
+
+// Output: [0,1,10]
+
+// Input: n = 3
+
+// Output: [0,1,10, 11]
+
 package main
 
 import "fmt"
