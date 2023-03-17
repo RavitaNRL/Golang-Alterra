d48ae0390d8b8fdb21fa3fb183ccfb137f71bc61
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-03-18 01:21:09 +0700

    TUGAS

--- 16_Join - Union - Agregasi - Subquery - Function (DBMS)/Praktikum/TUGAS2_JOIN.sql
+++ 16_Join - Union - Agregasi - Subquery - Function (DBMS)/Praktikum/TUGAS2_JOIN.sql
@@ -6,15 +6,61 @@ SELECT * FROM transaksi WHERE user_id = 1 OR user_id = 2;
 
 SELECT SUM(total_price) FROM transaksi WHERE user_id = 1;
 
+-- Tampilkan total transaksi dengan product type 2.
+
+SELECT SUM(total_price) as total_price FROM transaksi WHERE user_id = 1 group by user_id;
+
 -- Tampilkan semua field table product dan field name table products_type yang saling berhubungan.
 
-SELECT product.*, product_types.name FROM product JOIN product_types ON product.product_type_id = product_types.id;
+SELECT product.*, product_types.name FROM product 
+JOIN product_types ON product.product_type_id = product_types.id;
 
 -- Tampilkan semua field table transaction, field name table product dan field name table user.
 
-SELECT transaksi.*, product.name, users.name 
-FROM transactions
-INNER JOIN product ON transaksi.product_id = product.id
-INNER JOIN user ON transaksi.user_id = user.id;
+SELECT t.*, p.name, u.name as users FROM transaksi_details td 
+JOIN transaksi t ON t.id = td.transaksi_id
+JOIN users u ON u.id = t.user_id
+JOIN product p ON p.id = td.product_id
+WHERE t.user_id = 1;
+
+ -- Buat function setelah data transaksi dihapus maka transaction detail terhapus juga dengan transaction id yang dimaksud.
+
+delimiter $$
+CREATE trigger delete_transaksi_details
+BEFORE DELETE ON transaksi for each row
+BEGIN
+    DECLARE trans_id INT;
+    set trans_id = old.id;
+    DELETE FROM transaksi_details WHERE transaksi_id = trans_id;
+END $$
+
+DELETE FROM transaksi WHERE id = 4; 
+
+SELECT * FROM transaksi_details;
+
+-- Buat function setelah data transaksi detail dihapus maka data total_qty terupdate berdasarkan qty data transaction id yang dihapus.
+
+delimiter $$
+CREATE trigger update_transaksi_id 
+AFTER DELETE ON transaksi_details for each row
+BEGIN
+    DECLARE trans_id INT;
+    set trans_id = old.transaksi_id;
+    UPDATE transaksi set total_qty = (SELECT  SUM (qty) FROM transaksi_details WHERE transaksi_id = trans_id) 
+    WHERE id = trans_id;
+END $$ 
+
+SELECT * FROM transaksi_details;
+
+SELECT * FROM transaksi_details WHERE transaksi_id = 2;
+
+DELETE FROM transaksi_details WHERE transaksi_id = 2 AND product_id = 9;
+
+-- Tampilkan data products yang tidak pernah ada di tabel transaction_details dengan sub-query.
+
+SELECT * FROM product WHERE id NOT IN (SELECT product_id FROM transaksi_details);
+
+SELECT * FROM product;
+
 
 
